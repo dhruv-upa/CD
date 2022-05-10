ef6a95bd0f2dc390d8ff9cd3588af724e460967f
dhruv-upa <91604224+dhruv-upa@users.noreply.github.com>
2022-05-10 07:54:59 +0530

    Update FF.py

--- FF.py
+++ FF.py
@@ -2,7 +2,6 @@ import sys
 sys.setrecursionlimit(60)
 
 def first(string):
-    #print("first({})".format(string))
     first_ = set()
     if string in non_terminals:
         alternatives = productions_dict[string]
@@ -14,44 +13,38 @@ def first(string):
     elif string in terminals:
         first_ = {string}
 
-    elif string=='' or string=='@':
-        first_ = {'@'}
+    elif string=='' or string=='#':
+        first_ = {'#'}
 
     else:
         first_2 = first(string[0])
-        if '@' in first_2:
+        if '#' in first_2:
             i = 1
-            while '@' in first_2:
-                #print("inside while")
+            while '#' in first_2:
 
-                first_ = first_ | (first_2 - {'@'})
-                #print('string[i:]=', string[i:])
+                first_ = first_ | (first_2 - {'#'})
                 if string[i:] in terminals:
                     first_ = first_ | {string[i:]}
                     break
                 elif string[i:] == '':
-                    first_ = first_ | {'@'}
+                    first_ = first_ | {'#'}
                     break
                 first_2 = first(string[i:])
-                first_ = first_ | first_2 - {'@'}
+                first_ = first_ | first_2 - {'#'}
                 i += 1
         else:
             first_ = first_ | first_2
 
 
-    #print("returning for first({})".format(string),first_)
     return  first_
 
 
 def follow(nT):
-    #print("inside follow({})".format(nT))
     follow_ = set()
-    #print("FOLLOW", FOLLOW)
     prods = productions_dict.items()
     if nT==starting_symbol:
         follow_ = follow_ | {'$'}
     for nt,rhs in prods:
-        #print("nt to rhs", nt,rhs)
         for alt in rhs:
             for char in alt:
                 if char==nT:
@@ -63,12 +56,11 @@ def follow(nT):
                             follow_ = follow_ | follow(nt)
                     else:
                         follow_2 = first(following_str)
-                        if '@' in follow_2:
-                            follow_ = follow_ | follow_2-{'@'}
+                        if '#' in follow_2:
+                            follow_ = follow_ | follow_2-{'#'}
                             follow_ = follow_ | follow(nt)
                         else:
                             follow_ = follow_ | follow_2
-    #print("returning for follow({})".format(nT),follow_)
     return follow_
 
 
@@ -102,31 +94,18 @@ for _ in range(no_of_productions):
     productions.append(input())
 
 
-#print("terminals", terminals)
-
-#print("non terminals", non_terminals)
-
-#print("productions",productions)
-
-
 productions_dict = {}
 
 for nT in non_terminals:
     productions_dict[nT] = []
 
 
-#print("productions_dict",productions_dict)
 
 for production in productions:
     nonterm_to_prod = production.split("->")
     alternatives = nonterm_to_prod[1].split("/")
     for alternative in alternatives:
         productions_dict[nonterm_to_prod[0]].append(alternative)
-
-#print("productions_dict",productions_dict)
-
-#print("nonterm_to_prod",nonterm_to_prod)
-#print("alternatives",alternatives)
 
 
 FIRST = {}
@@ -138,19 +117,16 @@ for non_terminal in non_terminals:
 for non_terminal in non_terminals:
     FOLLOW[non_terminal] = set()
 
-#print("FIRST",FIRST)
 
 for non_terminal in non_terminals:
     FIRST[non_terminal] = FIRST[non_terminal] | first(non_terminal)
 
-#print("FIRST",FIRST)
 
 
 FOLLOW[starting_symbol] = FOLLOW[starting_symbol] | {'$'}
 for non_terminal in non_terminals:
     FOLLOW[non_terminal] = FOLLOW[non_terminal] | follow(non_terminal)
 
-#print("FOLLOW", FOLLOW)
 
 print("{: ^20}{: ^20}{: ^20}".format('Non Terminals','First','Follow'))
 for non_terminal in non_terminals:
