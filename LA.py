71a77328125f42ba50854c163f0f404a1cb52088
dhruv-upa <91604224+dhruv-upa@users.noreply.github.com>
2022-05-10 13:09:08 +0530

    Update LA.py

--- LA.py
+++ LA.py
@@ -1,49 +1,34 @@
-f=open(“/Users/dhruvupadhyay/Downloads/test.c")
-key=[‘int’,'float','string','include','stdio.h','char','break','if','else','switch','return', ‘void','while','struct','for']
+
+f=open(r"C:\Users\bawej\main.c")
+key=['int','float','string','include','stdio.h','char','break','if','else','switch','return','void' ,'while','struct','for']
 iden=[]
-sp={"(",")","{","}",";","&","#","$",'"',",","\n"}
-spec=["%d","%f","%c","%s"]
+sp={"(",")","{","}",";","&","#","$","\n",'"',","}
+spec=["%d","%f","%c","%s"] 
 num="012345678910"
 n=[]
 k=[]
 o=[]
-l=[]
-io=['scanf','printf']
+l=[] io=['scanf','printf','cin','cout'] 
 op="+-%*=/^><"
 dl=[]
 F=[]
 for lines in f:
 words=lines.split(" ")
 for i in range(len(words)):
-if words[i] in key:
-k.append(words[i])
-elif words[i] in io:
-l.append(words[i])
-elif words[i] in op:
-o.append(words[i])
-elif words[i] in sp:
-dl.append(words[i])
-elif words[i] in spec:
-F.append(words[i])
-elif words[i] in num:
-n.append(words[i])
-else:
-iden.append(words[i])
-
-print("Keywords: ")
-print(set(k))
-print("Input/Output: ")
-print(set(l))
-print("Operators: ")
-print(set(o))
-print("Special Symbols: ")
-print(set(dl))
-print("Identifiers: " )
-print(set(iden))
-print("Format Specifier:")
-print(set(F))
-print("Constants:")
-print(set(n))
+if words[i] in key: k.append(words[i])
+elif words[i] in io: l.append(words[i])
+elif words[i] in op: o.append(words[i])
+elif words[i] in sp: dl.append(words[i])
+elif words[i] in spec: F.append(words[i])
+elif words[i] in num: n.append(words[i])
+  
+else: iden.append(words[i])
+print("Keywords are: ") print(set(k)) print("input/output are: ") print(set(l))
+print("Operators are: ") print(set(o))
+print("Special Symbols are: ") print(set(dl))
+print("Identifiers are: " ) print(set(iden))
+print("Format Specifier are:") print(set(F))
+print("Constants are:") print(set(n))
 
 test.c
 #include <stdio.h>
